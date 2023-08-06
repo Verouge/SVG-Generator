30090a3ec2006a4450f99ed8119c77507d3c45b5
Verouge <branola1998628@gmail.com>
2023-08-06 21:07:23 +1000

    Updated Index.js and Shapes.js also added examples

--- index.js
+++ index.js
@@ -1,46 +1,69 @@
 const inquirer = require("inquirer");
-const generateSVGFile = require("./lib/shapes");
+const { Triangle, Circle, Square } = require("./lib/shapes");
+const fs = require("fs");
 
-function promptUser() {
-  return inquirer
-    .prompt([
-      {
-        type: "input",
-        name: "text",
-        message: "Enter up to three characters",
-        validate: (input) => {
-          input.length <= 3;
-        },
-      },
-      {
-        type: "input",
-        name: "textColor",
-        message: "Enter the text color (keyword or hexidecimal)",
-      },
-      {
-        type: "list",
-        name: "shape",
-        message: "What shape should it be?",
-        choices: ["square", "circle", "triangle"],
-      },
-      {
-        type: "input",
-        name: "shapeColor",
-        message: "Enter the shape color (keyword or hexidecimal)",
-      },
-    ])
-    .then((answers) => {
-      generateSVGFile(
-        answers.text,
-        answers.textColor,
-        answers.shape,
-        answers.shapeColor
-      );
+const generateSVG = (text, textColor, shapeType, shapeColor) => {
+  let shape;
+  switch (shapeType) {
+    case "triangle":
+      shape = new Triangle(shapeColor);
+      break;
+    case "circle":
+      shape = new Circle(shapeColor);
+      break;
+    case "square":
+      shape = new Square(shapeColor);
+      break;
+    default:
+      throw new Error("Invalid shape type");
+  }
+
+  return `
+    <svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+        ${shape.toSVG()}
+        <text x="150" y="115" font-family="Arial" font-size="40" fill="${textColor}" text-anchor="middle">${text}</text>
+    </svg>`;
+};
+
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "text",
+      message: "Enter a text (up to three characters):",
+      validate: (input) => input.length <= 3,
+    },
+    {
+      type: "input",
+      name: "textColor",
+      message: "Enter the text color (color keyword or hexadecimal):",
+      // Add validation for color keyword or hexadecimal
+    },
+    {
+      type: "list",
+      name: "shape",
+      message: "Choose a shape:",
+      choices: ["circle", "triangle", "square"],
+    },
+    {
+      type: "input",
+      name: "shapeColor",
+      message: "Enter the shape color (color keyword or hexadecimal):",
+      // Add validation for color keyword or hexadecimal
+    },
+  ])
+  .then((answers) => {
+    const svgContent = generateSVG(
+      answers.text,
+      answers.textColor,
+      answers.shape,
+      answers.shapeColor
+    );
+
+    if (svgContent) {
+      fs.writeFileSync("logo.svg", svgContent);
       console.log("Generated logo.svg");
-    })
-    .catch((error) => {
-      console.error("Error: ", error);
-    });
-}
-
-promptUser();
+    } else {
+      console.error("Error generating SVG content");
+    }
+  });
